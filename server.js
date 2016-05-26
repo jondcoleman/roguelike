4e6d56829b7645188e71ce3ec26806742256c35e
jondcoleman <jcoleman3307@gmail.com>
2016-05-26 08:40:22 -0400

    serve static files

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
+app.use(express.static('css'))
+app.use(express.static('dist'))
+app.use(express.static('img'))
+
 const port = process.env.PORT || 3000
 
 app.listen(port)
