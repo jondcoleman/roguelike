96da423f2116b7a73218045e3162e1c30325997b
jondcoleman <jcoleman3307@gmail.com>
2016-05-26 08:37:29 -0400

    removing gulp serve functions

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,27 +3,12 @@ import sass from 'gulp-sass'
 import babelify from 'babelify'
 import source from 'vinyl-source-stream'
 import browserify from 'browserify'
-import browserSync from 'browser-sync'
 import streamify from 'gulp-streamify'
 import uglify from 'gulp-uglify'
-
-gulp.task('serve', ['sass'], () => {
-  browserSync.init({
-    server: {
-      baseDir: './',
-      index: 'index.html'
-    }
-  })
-
-  gulp.watch('sass/*.scss', ['sass'])
-  gulp.watch('src/**/*.js', ['js'])
-  gulp.watch('/*.html').on('change', browserSync.reload)
-})
 
 gulp.task('sass', () => gulp.src('./sass/**/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./css'))
-  .pipe(browserSync.stream())
 )
 
 gulp.task('js', () => {
@@ -36,7 +21,6 @@ gulp.task('js', () => {
     .pipe(source('app.js'))
     .pipe(streamify(uglify()))
     .pipe(gulp.dest('./dist'))
-    .pipe(browserSync.stream())
 })
 
 gulp.task('build', ['js', 'sass'])
