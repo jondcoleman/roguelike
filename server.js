bbbea4369d4987889cbda511c051813013199e12
jondcoleman <jcoleman3307@gmail.com>
2016-05-26 08:44:01 -0400

    modify static paths

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.use(express.static('css'))
-app.use(express.static('dist'))
-app.use(express.static('img'))
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/dist', express.static(__dirname + '/dist'))
+app.use('/img', express.static(__dirname + '/img'))
+app.use('/resources', express.static(__dirname + '/resources'))
 
 const port = process.env.PORT || 3000
 
