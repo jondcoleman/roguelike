ca2db0c74f2d82371443789a4d8af2a2c621f001
Jonathan Coleman <jcoleman3307@gmail.com>
2023-09-21 20:46:38 -0700

    update things to make it work

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,28 +1,30 @@
-import gulp from 'gulp'
-import sass from 'gulp-sass'
-import babelify from 'babelify'
-import source from 'vinyl-source-stream'
-import browserify from 'browserify'
-import streamify from 'gulp-streamify'
-import uglify from 'gulp-uglify'
+import gulp from "gulp";
+import babelify from "babelify";
+import source from "vinyl-source-stream";
+import browserify from "browserify";
+import streamify from "gulp-streamify";
+import uglify from "gulp-uglify";
+const sass = require("gulp-sass")(require("sass"));
 
-gulp.task('sass', () => gulp.src('./sass/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./css'))
-)
+gulp.task("sass", () => {
+  return gulp
+    .src("./sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
 
-gulp.task('js', () => {
-  browserify({
-    entries: './src/app.js',
-    debug: true
+gulp.task("js", () => {
+  return browserify({
+    entries: "./src/app.js",
+    debug: true,
   })
     .transform(babelify)
     .bundle()
-    .pipe(source('app.js'))
+    .pipe(source("app.js"))
     .pipe(streamify(uglify()))
-    .pipe(gulp.dest('./dist'))
-})
+    .pipe(gulp.dest("./dist"));
+});
 
-gulp.task('build', ['js', 'sass'])
+gulp.task("build", gulp.series("js", "sass"));
 
-gulp.task('default', ['js', 'serve'])
+//gulp.task("default", gulp.series("js", "serve"));
